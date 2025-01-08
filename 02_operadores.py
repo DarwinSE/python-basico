b8138726ee6bf2df1411bc08ac43085cbb43ee45
Darwin Guerra <darwin.guerra@seneca-corp.com>
2025-01-08 12:22:06 -0500

    Lesion 3 operadores

---
+++ 02_operadores.py
@@ -0,0 +1,40 @@
+# Ejemplos de Todos Operadores
+
+# Aritmeticos
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(3 / 5)
+print(3 % 5)
+print(3 ** 5)
+print(10 // 3)
+
+print('Hola ' + 'Mundo!')
+print('Hola ' + str(2))
+print('Hola ' * 3)
+
+# Comparativos
+print(3 > 5)
+print(3 < 5)
+print(3 >= 5)
+print(3 <= 5)
+print(3 == 5)
+print(3 != 5)
+
+print('Hola' > 'Mundo')
+print('Hola' < 'Mundo')
+print('Hola' >= 'Mundo')
+print('Hola' <= 'Mundo')
+print('Hola' == 'Mundo')
+print('Hola' != 'Mundo')
+print('Hola' >= 'Zundo') # Ordenacion alfabetica por ASCII
+
+# Lógicos
+print(True and False)
+print(True or False)
+print(False and False)
+print(False or False)
+print(True and True)
+print(True or True)
+print(not(False))
+print(not(True))
