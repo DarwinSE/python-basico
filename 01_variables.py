a4ef96b633f1bb4cdedc9498f022178ce4aad799
Darwin Guerra <darwin.guerra@seneca-corp.com>
2025-01-08 10:32:36 -0500

    Ejercicios con variables

--- 01_variables.py
+++ 01_variables.py
@@ -1,5 +1,16 @@
 # Introduccion a variables
 
 my_variable = 'My variable string'
-
 print(my_variable)
+
+int_variable = 14
+print(int_variable)
+
+bool_variable = False
+print(bool_variable)
+
+# Concatenacion de variables
+print(my_variable, int_variable, bool_variable)
+
+print(str(int_variable))
+print(type(str(int_variable)))
